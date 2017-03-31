d0d6c490e8ce081c8a7b2a4c4a86a2a61715410c
Dhiraj <ddw147@gmail.com>
2017-03-31 18:19:28 +0530

    added entrust to project for roles and permissions

--- app/User.php
+++ app/User.php
@@ -10,7 +10,7 @@ use App\Notifications\OtpNotification;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    use EntrustUserTrait;
     /**
      * The attributes that are mass assignable.
      *
